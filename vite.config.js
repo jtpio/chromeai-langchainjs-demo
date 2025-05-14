81125506c5e2440364ecf9952cced865a966cc53
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2025-05-14 09:45:23 +0200

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 
-// Extract the repo name from the GitHub repository URL
-// This assumes your repo is at github.com/jtpio/chromeai-langchain-demo
-const repoName = 'chromeai-langchain-demo'
+const repoName = 'chromeai-langchainjs-demo'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
